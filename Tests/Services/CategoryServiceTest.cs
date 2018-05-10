e30181404f110462920d2333f0177dc1d5c03711
Lewis Jessett <lewis.jessett@solarwinds.com>
2018-05-10 22:31:19 +0100

    Started to test category service.

---
+++ Tests/Services/CategoryServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using BankStatementApi.Repositories;
+using BankStatementApi.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Tests.Services
+{
+    [TestClass]
+    class CategoryServiceTest
+    {
+        private Mock<CategoryRepository> mockCategoryRepository;
+        private CategoryService testSubject;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mockCategoryRepository = new Mock<CategoryRepository>();
+            testSubject = new CategoryService(mockCategoryRepository.Object);
+        }
+
+        [TestMethod]
+        public void GetCategoryForTransactionName_ReturnsACategoryForATransactionName_WhenACategoryIsFound()
+        {
+            var response = testSubject.GetCategoryForTransactionName("Cake");
+        }
+    }
+}
